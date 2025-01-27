8bc746d91ddb5d52ad04e51d451903ca44a2f1c3
patolesahil5 <patolesahil5@gmail.com>
2025-01-27 19:05:58 +0530

    Included Url in main Project

--- companyapi/urls.py
+++ companyapi/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from companyapi.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home", homepage)
+    path("home", homepage),
+    path("api/vi/", include('api.urls'))
 ]
