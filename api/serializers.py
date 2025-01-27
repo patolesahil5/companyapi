54668f54228da56adce54733a492ed3edf7c1fd0
patolesahil5 <patolesahil5@gmail.com>
2025-01-27 19:07:50 +0530

    Typo corrected

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@
 from api.models import Company
 from rest_framework import serializers
 
-class CompanySerializers(serializers.HyperLinkedModelSerializer):
+class CompanySerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Company
         fields="__all__"
