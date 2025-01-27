106aa9b5904d881455f586075f1b5554811ea9f4
patolesahil5 <patolesahil5@gmail.com>
2025-01-27 13:16:09 +0530

    Added ORM Columns

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#creating company model
+
+class Company(models.Model):
+    company_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    location=models.CharField(max_length=50)
+    about=models.TextField()
+    type=models.CharField(max_length=100, choices=(('IT','IT',),
+                                                   ('NON IT, NON IT'), ('MOBILE PHONES', 'MOBILE PHONES')
+                                                   ))
+    added_date=models.DateTimeField(auto_now=True)
+    active=models.BooleanField(default=True)
+
