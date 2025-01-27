5e566cc83241b57642ce08ecc21d298bce55d8a0
patolesahil5 <patolesahil5@gmail.com>
2025-01-27 19:29:03 +0530

    Updated Spelling Mistake

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import Company
-from api.serializers import CompanySerializer
+from api.serializers import CompanySerializers
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet()):
     queryset=Company.objects.all()
-    serializer_class=CompanySerializer
+    serializer_class=CompanySerializers
